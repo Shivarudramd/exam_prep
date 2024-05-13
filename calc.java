51b6d1c68a26b0e8ea0be6f8d782da45cf474977
Shivarudra <shivarudradodamani@gmail.com>
2024-05-13 20:40:40 +0530

    java pgms

---
+++ calc.java
@@ -0,0 +1,15 @@
+public class calc {
+    public static void main(String[] args) {
+        int number1, number2, result;
+        number1=20;
+        number2=10;
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+          }
+}
